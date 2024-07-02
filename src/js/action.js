daf1247a0734078b2aa570beff412449c295b577
Jakub Barszcz <j.barszcz@op.pl>
2024-07-02 21:36:32 +0200

    working UI and iframe URL switching for different lists. To-do is backend and the list UIs.

--- src/js/action.js
+++ src/js/action.js
@@ -1,6 +1,9 @@
-function activateItem(elid) {
-  let clickedElement = document.getElementById(elid);
-  let activeElement = document.getElementsByClassName("active");
-  activeElement.classList.remove("active");
-  clickedElement.classList.add("active");
+function updateMenu(activeItem){
+    let itemsList = ['lmi0', 'lmi1', 'lmi2', 'lmi3', 'lmi4', 'lmi5', 'lmi6', 'lmi7'];
+    let sourcesList = ['https://weather-pro-v2fr6.ondigitalocean.app/', 'https://weather-pro-v2fr6.ondigitalocean.app/', 'https://weather-pro-v2fr6.ondigitalocean.app/', 'https://weather-pro-v2fr6.ondigitalocean.app/']
+    for(i = 0; i < itemsList.length; i++){
+        document.getElementById(itemsList[i]).classList.remove('active');
+    }
+    document.getElementById(itemsList[activeItem]).classList.add('active');
+    document.getElementById("currentWindow").src = sourcesList[activeItem];
 }
